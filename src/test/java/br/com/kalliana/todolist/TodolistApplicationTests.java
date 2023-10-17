c28947871a0ec1ddf7c7cfedc2d70bb427044cd5
KALLIANA GLAESER <kalliana.glaeser@gmail.com>
2023-10-16 23:14:58 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/kalliana/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kalliana.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
